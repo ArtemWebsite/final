c6321808e51123c5429bfd4a0ba37133b1b0a4d6
Prowebcoder <rahul@prowebcoder.com>
2024-11-09 21:02:10 +0530

    new changes

--- components/pages/cems/cems-banner.jsx
+++ components/pages/cems/cems-banner.jsx
@@ -25,71 +25,82 @@ const BannerOne = () => {
   return (
     <>
       <div className={styles.banner__one}>
-      <Swiper modules={[EffectFade, Autoplay, Navigation]} {...slideControl}>
-      <SwiperSlide>
-            <div className={styles.banner__one_image_1}>
+        <Swiper modules={[EffectFade, Autoplay, Navigation]} {...slideControl}>
+          <SwiperSlide>
+            <div
+              className={styles.banner__one_image_1}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                height: "85vh",
+              }}
+            >
               <img
                 className={styles.banner__one_shape}
                 src={bannerBg.src}
                 alt="banner-shape"
               />
-              
-              <div className="container" style={{zIndex:"999"}}>
-              <div className="row d-flex align-items-stretch">
 
-<div className="col-xl-12 pt-60 pb-10 text-center">
+              <div className="container" style={{ zIndex: "999" }}>
+                <div className="row d-flex align-items-stretch">
+                  <div className="col-xl-12 pt-60 pb-10 text-center">
                     <div className={styles.banner__one_content}>
                       <h2 className="text-light pb-30  width-narrow">
-                      Emission Monitoring Equipment
+                        Emission Monitoring Equipment
                       </h2>
-                      <h6 className="text-light pb-30  width-narrow">The ARTEM CEMS Modul is designed to continuously monitor multiple parameters, including the content of CO2 SO2, NO, O2 and Perfluoro carbons.  It measures:  </h6>
+                      <h6 className="text-light pb-30  width-narrow">
+                        The ARTEM CEMS Modul is designed to continuously monitor
+                        multiple parameters, including the content of CO2 SO2,
+                        NO, O2 and Perfluoro carbons. It measures:{" "}
+                      </h6>
                     </div>
                   </div>
-<div className="width-narrow-new row ">
-  <div className="col-lg-3 col-md-6 text-center ">
-      <div className="btn-four">
-      Flow Rate
-      </div>
-  </div>
+                  <div className="width-narrow-new row ">
+                    <div className="col-lg-3 col-md-6 text-center ">
+                      <div className="btn-four">Flow Rate</div>
+                    </div>
 
-  <div className="col-lg-3 col-md-6 text-center">
-      <div className="btn-four" >
-      Pressure
-      </div>
-  </div>
+                    <div className="col-lg-3 col-md-6 text-center">
+                      <div className="btn-four">Pressure</div>
+                    </div>
 
-  <div className="col-lg-3 col-md-6 text-center">
-      <div className="btn-four" >
-      Humidity of the Flue Gas
-      </div>
-  </div>
+                    <div className="col-lg-3 col-md-6 text-center">
+                      <div className="btn-four">Humidity of the Flue Gas</div>
+                    </div>
 
-  <div className="col-lg-3 col-md-6 text-center">
-      <div className="btn-four" >
-      Temperature
-      </div>
-  </div>
-  </div>
-  <div className="col-xl-12 pt-0 text-center">
+                    <div className="col-lg-3 col-md-6 text-center">
+                      <div className="btn-four">Temperature</div>
+                    </div>
+                  </div>
+                  <div className="col-xl-12 pt-0 text-center">
                     <div className={styles.banner__one_content}>
-                      <p>The measured parameters are transmitted to the Data Storage System(DSS). </p>
+                      <p>
+                        The measured parameters are transmitted to the Data
+                        Storage System(DSS).{" "}
+                      </p>
                     </div>
                   </div>
-</div>
-              <div className="row d-flex justify-content-center">
-              <div className="col-lg-8 col-md-6 text-center">
-                  <img className={styles.banner__one_content} src={img1.src} alt="img-with-text" />
+                </div>
+                <div className="row d-flex justify-content-center">
+                  <div className="col-lg-8 col-md-6 text-center">
+                    {/* <img className={styles.banner__one_content} src={img1.src} alt="img-with-text" /> */}
                   </div>
-                  </div>
+                </div>
               </div>
-              <div className="banner-image-overlay" style={{    position: "absolute",
-    height: "100%",
-    width:" 100%",
-    background: "#00000082",
-    zIndex: "99"}}></div>
+              <div
+                className="banner-image-overlay"
+                style={{
+                  position: "absolute",
+                  height: "100%",
+                  width: " 100%",
+                  background: "#00000082",
+                  zIndex: "99",
+                  top: "0",
+                }}
+              ></div>
             </div>
-            </SwiperSlide>
-            </Swiper>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </>
   );
--- components/pages/homes/home/styles/image-with-text.jsx
+++ components/pages/homes/home/styles/image-with-text.jsx
@@ -1,37 +1,36 @@
-import img1 from "../../../../../public/assets/img/cems/small-image-banner.png";
-import img2 from "../../../../../public/assets/img/cems/unit.jpg";
+// import img1 from "../../../../../public/assets/img/cems/small-image-banner.png";
+import img2 from "../../../../../public/assets/img/cems/mm.png";
 import MeasurementTable from "../measuring";
 
 const ImageWithText = () => {
-    
-    return (
-        <div className="team__details pt-60">
-            <div className="container">
-            <div className="row">
-            <div className="col-xl-12 pb-5">
-              <h3 className="text-center">Our Model’s Measurements</h3>
-            </div>
+  return (
+    <div className="team__details pt-60">
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-12 pb-5">
+            <h3 className="text-center">Our Model’s Measurements</h3>
           </div>
-                <div className="row">
-          <div className="col-md-7">
-                            <MeasurementTable />
-          </div>
-                    <div className="col-md-5">
-                    <div className="col-xl-12">
-                    <div className="row">
-                    <div className="col-md-6">
-            <img className="" src={img1.src} alt="image" />
-            </div>
-            <div className="col-md-6">
-            <img className="" src={img2.src} alt="image" />
-            </div>
-            </div>
-</div>
-          </div>
-                </div>
-            </div>
         </div>
-    );
+        <div className="row">
+          <div className="col-md-7">
+            <MeasurementTable />
+          </div>
+          <div className="col-md-5">
+            <div className="col-xl-12" style={{ height: "100%" }}>
+              <div
+                className="row align-center"
+                style={{ height: "100%", alignItems: "center" }}
+              >
+                <div>
+                  <img className="" src={img2.src} alt="image" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default ImageWithText; 
+export default ImageWithText;
